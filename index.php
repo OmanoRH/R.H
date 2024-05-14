175cc478088caaafb367e2875f29c777015f523e
OmanoRH <renatohgsoares@gmail.com>
2024-05-14 17:02:29 -0300

    tarefa de casa

--- index.php
+++ index.php
@@ -1,28 +1,104 @@
 <!DOCTYPE html>
-    <html lang="en">
+    <html lang="pt-br">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <h1>
-            pindamongoiaba
-        </h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore, consequatur nihil molestiae accusamus, cupiditate exercitationem ex in et nostrum ea quo non esse laudantium officiis architecto. Quaerat voluptatum corporis recusandae?</p>
         <?php
-        $nome = "A";
-        echo "<h2>$nome</h2>";
+
+        $DesenhosDaCartoon = array("Ben 10", "Hora de Aventura", "Apenas um Show", "historias macabras para crianças malcriadas", "Irmão do Jorel", "O Incrivel Mundo de Gumbool");
+        $a = ["Janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho"];
+        $DesenhosDaCartoon[3] = "As trapalhadas de Flap Jack";
+        foreach($DesenhosDaCartoon as $a){
+            echo ("$a <br>");
+        
+        }
+        for ($b = 80; $b >= 20; $b-= 2 ){
+            $divi = $b/2;
+            echo ("$divi <br>");
+        };
         ?>
 
-        <?php
-        function sub($x, $y){
-        $subitrair = $x - $y;
+        <br>
+        <br>
         
-        return $subitrair;
+        <?php
+
+        $x = 2;
+        $y = 4;
+        $soma = $x + $y;
+        $resultado = $soma * $x;
+
+        echo ($resultado)
+
+        ?>
+
+        <br><br>
+
+        <?php
+
+        $val1 = 7;
+        $val2 = 9;
+        $val3 = 4;
+        $soma = $val1 + $val2 + $val3;
+        $resultado = $soma / 3;
+
+        echo ($resultado)
+
+        ?>
+
+        <br> <br>
+
+        <?php
+
+        $valor = 100;
+        $resultado = $valor * 0.15; 
+
+        echo ($resultado);
+
+        ?>
+
+        <br><br>
+
+        <?php
+
+        $valor = 100;
+        $resultado5 = $valor * 0.05; 
+        $resultado50 = $valor * 0.50;
+
+        echo ("$resultado5 $resultado50");
+
+        ?>
+
+        <br><br>
+
+        <?php
+        $x = 0;
+        if($x >0){
+            echo ("Valor Positivo");
+        }
+        elseif($x <0){
+            echo ("Valor Negativo");
+        }
+        else{
+            echo ("Igual a Zero");
         }
 
-        echo (sub(15, 10));
         ?>
-    </body>
+
+        <?php
+
+        for($i = 0; $i >= 10; $i++){
+            $multi = 4 * $i;
+            echo $multi;
+        }
+
+        ?>
+
+        <?php
+        ?>
+
 </html>
